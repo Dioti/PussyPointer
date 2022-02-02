9c3d902519510a045e4496f7ddcd2de725f3f339
Natasha <natasha.pearl08@gmail.com>
2022-02-02 14:34:36 +0000

    added basic mouse tracker in js

--- src/main/resources/static/js/mouse.js
+++ src/main/resources/static/js/mouse.js
@@ -1,3 +1,8 @@
-onmousemove = function(e){
+var timer;
+window.addEventListener("mousemove",function(e){
+    clearTimeout(timer);
     console.log(e.clientX + ", " + e.clientY);
-}
+    timer=setTimeout(function () {
+        console.log("MOUSE STOPPED");
+    },3000);
+});
