4a393a67e72fa75458d8c95bd9f6e0abde115ee9
Natasha <natasha.pearl08@gmail.com>
2022-02-02 11:41:48 +0000

    Setup initial project

---
+++ src/main/java/com/dioti/pussypointer/PussyPointerApplication.java
@@ -0,0 +1,13 @@
+package com.dioti.pussypointer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PussyPointerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PussyPointerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dioti/pussypointer/PussyPointerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dioti.pussypointer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PussyPointerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
